906d25d7d08b4d4be828686fd20ff250bcdc93d5
abschill <ajay198810@gmail.com>
2022-01-05 20:42:15 -0500

    comment example

--- example/main.go
+++ example/main.go
@@ -6,13 +6,20 @@ import (
 )
 
 func main() {
+	//Call up.DefaultLoaderOptions() to stick with the default options 
 	options := up.CustomLoaderOptions( "root", "pages", "partials", false, false )
+
+	//Define Loader with the generated LoaderOptions
 	loader := up.DefineLoader( options )
+
+	//Create a Slice for Template Data
 	var data = make( []up.Input, 1 )
+	//Fill in our page_title example as an Input
 	data[0] = up.Input{
 		Key: "page_title",
 		Value: "Underpin",
 	}
+	//Retrieve the Template result from loader
 	fmt.Println( loader.Template( "home", data ) );
 	
 }
