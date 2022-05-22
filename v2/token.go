df8ff1f0098261f6c80974006065042e62d1f3b9
abschill <ajay198810@gmail.com>
2022-05-21 21:23:41 -0600

    update: tokens

--- v2/token.go
+++ v2/token.go
@@ -3,9 +3,15 @@ package main
 type IType string
 
 const (
-	ENDL IType = "end"
-	CALL IType = "call"
-	SET  IType = "set"
+	ISTART IType = "start" // start open chunk scope
+	_ISET  IType = "macro" // set metadata about chunk type
+	IOPEN  IType = "open"  // start open expr scope inside chunk
+	ICLOSE IType = "close" // close IOPEN scope
+	IBRK   IType = "break" // break line & any existing scope
+	ICALL  IType = "call"  // call directive as argument
+	ISET   IType = "set"   // set directive input to argument follow up
+	IWRAP  IType = "wrap"  // wrap expression with a condition or configuration hook
+	IEND   IType = "end"   // end the html chunk scope
 )
 
 type HTMLCToken struct {
@@ -15,19 +21,24 @@ type HTMLCToken struct {
 	POptions        []HTMLCToken
 }
 
+var AssignedTokens = 0
+
 // token that is resolved within a chunk
 type HTMLCResolvedToken struct {
+	Token     HTMLCToken
 	StartLine int
 	EndLine   int
 	StartCol  int
 	EndCol    int
+	FromScope string
+	FromChunk string
 }
 
 func (HTMLCToken) Exists() bool {
 	return false
 }
 
-func (HTMLCToken) Get() HTMLCResolvedToken {
+func (HTMLCToken) Get(input string) HTMLCResolvedToken {
 	return HTMLCResolvedToken{}
 }
 
@@ -35,22 +46,23 @@ func (HTMLCToken) Replace() bool {
 	return false
 }
 
-func tokenize(name string, sig string) HTMLCToken {
+func tokenize(name string, sig string, t IType) HTMLCToken {
 	return HTMLCToken{
-		Name:      name,
-		Signature: sig,
+		Name:            name,
+		Signature:       sig,
+		InstructionType: t,
 	}
 }
 
 var HTMLC_TOKENS = []HTMLCToken{
-	tokenize("HTML_OC_SCOPE", "<!--@htmlc|"),
-	tokenize("HTML_CC_SCOPE", "|@htmlc-->"),
-	tokenize("HTMLC_TD_OSCOPE", "("),
-	tokenize("HTMLC_TD_CSCOPE", ")"),
-	tokenize("HTMLC_TD_RENDER", "#render"),
-	tokenize("HTMLC_TD_CHUNK", "#chunk"),
-	tokenize("HTMLC_TO_SET", "="),
-	tokenize("HTMLC_TD_ENFORCE", "!"),
-	tokenize("HTMLC_TD_TRY", "?"),
-	tokenize("HTMLC_CM_PREFIX", "~"),
+	tokenize("HTML_OC_SCOPE", "<!--@htmlc|", ISTART),
+	tokenize("HTML_CC_SCOPE", "|@htmlc-->", IEND),
+	tokenize("HTMLC_CM_PREFIX", "~", _ISET),
+	tokenize("HTMLC_TD_OSCOPE", "(", IOPEN),
+	tokenize("HTMLC_TD_CSCOPE", ")", ICLOSE),
+	tokenize("HTMLC_TD_RENDER", "#render", ICALL),
+	tokenize("HTMLC_TD_CHUNK", "#chunk", ICALL),
+	tokenize("HTMLC_TO_SET", "=", ISET),
+	tokenize("HTMLC_TD_ENFORCE", "!", IWRAP),
+	tokenize("HTMLC_TD_TRY", "?", IWRAP),
 }
